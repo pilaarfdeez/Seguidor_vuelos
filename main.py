f67474416f39b3cb81012a71372f68509a578f91
DavidMayoral <davidmayoral1505@gmail.com>
2025-03-08 13:11:57 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from src.google_flight_analysis.scrape import *
+
+
+result = Scrape('JFK', 'IST', '2023-07-20', '2023-08-20') # obtain our scrape object, represents out query
+# result.type # This is in a round-trip format
+# result.origin # ['JFK', 'IST']
+# result.dest # ['IST', 'JFK']
+# result.dates # ['2023-07-20', '2023-08-20']
+print(result) # get unqueried str representation
+
+ScrapeObjects(result) # runs selenium through ChromeDriver, modifies results in-place
+print(result.data) # returns pandas DF
+print(result) # get queried representation of result
