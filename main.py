2c41ca8e16a9927b4ade7b961fc28e2d4dffdba7
DavidMayoral <davidmayoral1505@gmail.com>
2025-03-09 18:02:53 +0100

    Updated main file for testing in GitHub Actions

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from src.google_flight_analysis.scrape import *
 
 
-result = Scrape('AGP', 'HAM', '2025-07-20', '2025-08-20') # obtain our scrape object, represents out query
-# result.type # This is in a round-trip format
-# result.origin # ['JFK', 'IST']
-# result.dest # ['IST', 'JFK']
-# result.dates # ['2023-07-20', '2023-08-20']
+result = Scrape('AGP', 'HAM', '2025-05-15', '2025-05-20') # obtain our scrape object, represents out query
+
 print(result) # get unqueried str representation
 
-ScrapeObjects(result) # runs selenium through ChromeDriver, modifies results in-place
+ScrapeObjects(result, headless=True) # runs selenium through ChromeDriver, modifies results in-place
 print(result.data) # returns pandas DF
 print(result) # get queried representation of result
+
+print('Loading the search results in flights.json')
+result.data.to_json('data/flights.json', orient='records', indent=4)
+print('Scraper jobs terminated successfully!')
