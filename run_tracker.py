83b51276c835cbb13af24511a15da369fbde1caa
pilaarfdeez <157853388+pilaarfdeez@users.noreply.github.com>
2025-04-24 12:06:13 +0200

    Update run_tracker.py
    Change order to not look for the flight and only send the message

--- run_tracker.py
+++ run_tracker.py
@@ -34,6 +34,8 @@ for flight in conf.FLIGHTS_TO_REMOVE:
     tracked_flight = TrackedFlight(flight)
     tracker.delete_flight(tracked_flight)
 
+send_auto_message()
+
 for key, flights in tracker.group_flights().items():
     logger.info(f'Checking {len(flights)} tracked flights for {key}')
     result = Scrape(key[1], key[2], key[0])
@@ -57,6 +59,5 @@ logger.info('Tracker jobs terminated successfully!')
 logger.info('Sending email...')
 updated_flights = tracker.new_prices()
 reporter.send_report(updated_flights, conf.ENV)
-print("aqui estoy")
-send_auto_message()
+
 
