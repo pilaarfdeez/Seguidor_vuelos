3a76877df28186d2815c3df29711199b78c2abbe
pilaarfdeez <157853388+pilaarfdeez@users.noreply.github.com>
2025-05-27 21:15:58 +0200

    Update config.py

--- config/config.py
+++ config/config.py
@@ -27,7 +27,7 @@ class BargainFinderConfig:
     def __init__(self):
         self.ENV = env
 
-        self.WEEK_START = 7     # In how many weeks from now should the Finder start searching? 
+        self.WEEK_START = 5     # In how many weeks from now should the Finder start searching? 
         self.WEEKS_SEARCH = 10  # How many weeks from the start week should the Finder search?
 
         self.AIRPORTS_PILAR = (['AGP','GRX'], ['HAM','BRE','LBC'])
