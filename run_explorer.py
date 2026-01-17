260037db3724b1bc2664947a88332173b2e501ef
DavidMayoral <davidmayoral1505@gmail.com>
2026-01-17 17:31:54 +0100

    Implement explorer return flight search

--- run_explorer.py
+++ run_explorer.py
@@ -8,7 +8,7 @@ from src.destination_explorer.explorer import Explorer
 # from src.destination_explorer.match import Match
 from src.google_flight_analysis.human_simulations import *
 from src.google_flight_analysis.scrape import *
-from report.report import FlightMatchReporter
+from src.report.report import FlightMatchReporter
 
 conf = ExplorerConfig()
 explorer = Explorer(conf)
@@ -22,42 +22,57 @@ elif conf.ENV == 'local':
 
 
 today_date = dt.date.today()
-country_codes_path = "data/country_codes.json"
-with open(country_codes_path, "r", encoding="utf-8") as f:
-    country_codes = json.load(f)
 
 for day in conf.DAYS_DEPARTURE:
     if dt.datetime.strptime(day, "%Y-%m-%d").date() < today_date:
         logger.warning(f"Date {day} is in the past --> Skipping.")
         continue
-    for country, code in country_codes.items():
+    for country, code in explorer.country_to_fb.items():
         logger.info(f"Exploring matching outbound flights to {country} on {day}")
         destination = code
         results_david = Scrape(conf.AIRPORT_DAVID, [destination], day)
         results_pilar = Scrape(conf.AIRPORT_PILAR, [destination], day)
         ScrapeObjects(results_david, conf.ENV)
         ScrapeObjects(results_pilar, conf.ENV)
-        explorer.process_matches(day, country, results_david.data, results_pilar.data)
+
+        processed_matches = explorer.process_matches(day, country, results_david.data, results_pilar.data)
+        explorer.potential_matches[0] = pd.concat([explorer.potential_matches[0], processed_matches], axis=0)
+        logger.info(f"--> {len(processed_matches)} potential matches found!")
+
         random_wait(min_sec=0.1, max_sec=1)
 
     random_wait(min_sec=10, max_sec=15)
 
-# for day in conf.DAYS_RETURN:
-#     if dt.datetime.strptime(day, "%Y-%m-%d").date() < today_date:
-#         logger.warning(f"Date {day} is in the past --> Skipping.")
-#         continue
-#     for country in explorer.potential_matches['Country'].unique():
-#         origin = explorer.potential_matches[explorer.potential_matches['Country'] == country]['City'].unique()
-#         # TODO: Get the freebase ID (alternatively airport code) for each of the cities
-#         results_david = Scrape([origin], conf.AIRPORT_DAVID, day)
-#         results_pilar = Scrape([origin], conf.AIRPORT_PILAR, day)
-#         ScrapeObjects(results_david, conf.ENV)
-#         ScrapeObjects(results_pilar, conf.ENV)
-#         explorer.verify_matches(results_david.data, results_pilar.data)
+explorer.get_freebase_ids()
 
+for country in explorer.potential_matches[0]['Country'].unique():
+    for day in conf.DAYS_RETURN:
+        if dt.datetime.strptime(day, "%Y-%m-%d").date() < today_date:
+            logger.warning(f"Date {day} is in the past --> Skipping.")
+            continue
+
+        logger.info(f"Exploring matching return flights from {country} on {day}")
+        origin = explorer.potential_matches[0][explorer.potential_matches[0]['Country'] == country]['City'].unique().tolist()
+        origin_ids = [explorer.city_to_fb[(city, country)] for city in origin if (city, country) in explorer.city_to_fb]
+        if conf.COUNTRY_TRIP:
+            # TODO: Explore matching return flights from any city
+            pass
+        for i in range(0, len(origin_ids), 6):
+            batch = origin_ids[i:i+6]
+            results_david = Scrape(batch, conf.AIRPORT_DAVID, day)
+            results_pilar = Scrape(batch, conf.AIRPORT_PILAR, day)
+            ScrapeObjects(results_david, conf.ENV)
+            ScrapeObjects(results_pilar, conf.ENV)
+
+            processed_matches = explorer.process_matches(day, country, results_david.data, results_pilar.data)
+            explorer.potential_matches[1] = pd.concat([explorer.potential_matches[1], processed_matches], axis=0)
+
+    random_wait(min_sec=2, max_sec=3)
+
+explorer.create_combinations()
 
 explorer.save_matches(matches='potential')
 logger.info('Explorer job terminated successfully!')
 
 logger.info('Sending email...')
-reporter.send_report()
+# reporter.send_report()
--- src/destination_explorer/explorer.py
+++ src/destination_explorer/explorer.py
@@ -3,21 +3,30 @@ import json
 import pandas as pd
 
 from config.logging import init_logger
+from src.google_flight_analysis.airport import Airport
+from src.google_flight_analysis.human_simulations import random_wait
 # from src.destination_explorer.match import Match
 
 logger = init_logger(__name__)
+airp_helper = Airport() 
 
 class Explorer:
     def __init__(self, conf):
         self.conf = conf
 
-        columns=['Day', 'Country', 'City', 'Price_David', 'Price_Pilar', 'Total_Price', 'Flight_Time_David', 'Flight_Time_Pilar', 'Stops_David', 'Stops_Pilar']
-        self.potential_matches = pd.DataFrame(columns=columns)
-        self.matches = pd.DataFrame(columns=columns)
+        self.match_columns=['Day', 'Country', 'City', 'Total_Price', 'Price_David', 'Price_Pilar', 'Travel_Time_David', 'Travel_Time_Pilar', 'Stops_David', 'Stops_Pilar']
+        self.potential_matches = [pd.DataFrame(columns=self.match_columns), pd.DataFrame(columns=self.match_columns)]
+        self.matches = pd.DataFrame(columns=self.match_columns)
+
+        with open('data/country_codes.json', "r", encoding="utf-8") as f:
+            self.country_to_fb = json.load(f)  # Dictionary mapping country names to Freebase IDs
+        with open('data/city_codes.json', "r", encoding="utf-8") as f:
+            self.city_to_fb = json.load(f)  # Dictionary mapping (city, country) tuples to Freebase IDs
+        self.missing_ids = []
 
     def to_dict(self, matches='real'):
         if matches == 'potential':
-            matches_df = self.potential_matches.copy()
+            matches_df = self.potential_matches[0].copy()
         elif matches == 'real':
             matches_df = self.matches.copy()
 
@@ -26,7 +35,7 @@ class Explorer:
             day_matches = matches_df[matches_df['Day'] == day].drop(columns=['Day'])
 
             # Format flight times as 'Xh Ymin'
-            def format_flight_time(td):
+            def format_travel_time(td):
                 total_minutes = pd.to_timedelta(td).total_seconds() // 60
                 hours = int(total_minutes // 60)
                 minutes = int(total_minutes % 60)
@@ -34,8 +43,8 @@ class Explorer:
             for col in day_matches.columns:
                 if 'Price' in col:
                     day_matches[col] = day_matches[col].apply(lambda x: f"{x}€")
-                elif 'Flight_Time' in col:
-                    day_matches[col] = day_matches[col].apply(format_flight_time)
+                elif 'Travel_Time' in col:
+                    day_matches[col] = day_matches[col].apply(format_travel_time)
 
             matches_dict.append({
                 'Day': day,
@@ -43,9 +52,11 @@ class Explorer:
             })
         return matches_dict
 
-    def sort_matches(self):
-        self.potential_matches = self.potential_matches.sort_values(by=['Day', 'Total_Price', 'Country', 'City'])
-        self.matches = self.matches.sort_values(by=['Day', 'Total_Price', 'Country', 'City'])
+    def sort_matches(self, matches='both'):
+        if matches in ['potential', 'both']:
+            self.potential_matches[0] = self.potential_matches[0].sort_values(by=['Day', 'Total_Price', 'Country', 'City'])
+        if matches in ['real', 'both']:
+            self.matches = self.matches.sort_values(by=['Total_Price', 'Day', 'Country', 'City'])
 
 
     def process_matches(self, day, country, results_david: pd.DataFrame, results_pilar: pd.DataFrame):
@@ -58,8 +69,8 @@ class Explorer:
         cross_joined = results_david.merge(results_pilar, on=match_keys, suffixes=('_David', '_Pilar'))
         filter = (
             (cross_joined['Price_David'] + cross_joined['Price_Pilar']  < 0.6 * self.conf.MAX_PRICE) &
-            (cross_joined['Flight_Time_David'] < dt.timedelta(hours=self.conf.MAX_TRAVEL_HOURS)) &
-            (cross_joined['Flight_Time_Pilar'] < dt.timedelta(hours=self.conf.MAX_TRAVEL_HOURS))
+            (cross_joined['Travel_Time_David'] < dt.timedelta(hours=self.conf.MAX_TRAVEL_HOURS)) &
+            (cross_joined['Travel_Time_Pilar'] < dt.timedelta(hours=self.conf.MAX_TRAVEL_HOURS))
         )
         joined_filtered = cross_joined[filter].copy()
         if joined_filtered.empty:
@@ -68,14 +79,81 @@ class Explorer:
         joined_filtered.loc[:, 'Day'] = day
         joined_filtered.loc[:, 'Country'] = country
         joined_filtered.loc[:, 'Total_Price'] = joined_filtered['Price_David'] + joined_filtered['Price_Pilar']
-        self.potential_matches = pd.concat([self.potential_matches, joined_filtered], axis=0)
-        logger.info(f"--> {len(joined_filtered)} potential matches found!")
+
+        return joined_filtered
 
 
-    def verify_matches(self, results_david, results_pilar):
-        pass
+    def create_combinations(self):
+        combinations_df = (
+            self.potential_matches[0].set_index(['City', 'Country'])
+            .merge(
+                self.potential_matches[1].set_index(['City', 'Country']), 
+                left_index=True, right_index=True, how='inner', suffixes=('_out', '_return')
+            )
+            .reset_index()
+        )
+
+        if combinations_df.empty:
+            return
+        
+        matches_df = pd.DataFrame({
+            'City': combinations_df['City'],
+            'Country': combinations_df['Country']
+        })
+
+        double_columns = [c for c in self.match_columns if f'{c}_out' in combinations_df.columns and f'{c}_return' in combinations_df.columns]
+        for col in double_columns:
+            if col == 'Total_Price':
+                matches_df[col] = (
+                    combinations_df[f'{col}_out']  #.str.replace('€', '').astype('int') 
+                    +
+                    combinations_df[f'{col}_return']  #.str.replace('€', '').astype('int')
+                )
+                continue
+            matches_df[col] = list(zip(combinations_df[f'{col}_out'], combinations_df[f'{col}_return']))
+
+        matches_df = matches_df[matches_df['Total_Price'] <= self.conf.MAX_PRICE]
+
+        self.matches = matches_df[self.match_columns]
+        self.sort_matches(matches='real')
+
 
     def save_matches(self, matches='real'):
         self.sort_matches()
         with open(f"data/{matches}_matches.json", "w+") as file:
             json.dump(self.to_dict(matches=matches), file, indent=4)
+
+
+    def get_freebase_ids(self, sleep=0.5, matches='potential'):
+        if matches == 'potential':
+            matches_df = self.potential_matches[0].copy()
+        elif matches == 'real':
+            matches_df = self.matches.copy()
+
+        unique_cities = matches_df[["City", "Country"]].drop_duplicates()
+        logger.info(f"Retrieving Freebase IDs for {len(unique_cities)} unique cities.")
+
+        for _, row in unique_cities.iterrows():
+            city = row["City"]
+            country = row["Country"]
+
+            if (city, country) in self.city_to_fb:
+                fb_id = self.city_to_fb[(city, country)]
+                logger.debug(f"Found cached FreebaseID for {city} ({fb_id})")
+                continue
+
+            try:
+                fb_id = airp_helper.to_freebase_id(city, country)
+                logger.debug(f"Received FreebaseID for {city} ({fb_id})")
+                if fb_id:
+                    self.city_to_fb[(city, country)] = fb_id
+
+            except Exception as e:
+                logger.warning(f"Error retrieving Freebase ID for {city}, {country}: {e}")
+                self.missing_ids.append((city, country))
+            random_wait(sleep, 1.05*sleep)  # polite rate limiting
+
+        # Sort and export dictionary for future use
+        self.city_to_fb = dict(sorted(self.city_to_fb.items(), key=lambda x: (x[0][1], x[0][0])))
+        with open("data/city_codes.json", "w+", encoding="utf-8") as f:
+            json.dump(self.city_to_fb, f, indent=4)
