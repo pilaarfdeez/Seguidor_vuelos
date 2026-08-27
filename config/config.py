838c09310a5eece02902508838767c1a27ba0c17
pilaarfdeez <157853388+pilaarfdeez@users.noreply.github.com>
2026-08-27 22:09:51 +0200

    Update airport codes for Pilar and David

--- config/config.py
+++ config/config.py
@@ -34,8 +34,8 @@ class BargainFinderConfig:
         self.WEEK_START = 4     # In how many weeks from now should the Finder start searching? 
         self.WEEKS_SEARCH = 10  # How many weeks from the start week should the Finder search?
 
-        self.AIRPORTS_PILAR = (['AGP','GRX'], ['HAM','BRE','LBC'])
-        self.AIRPORTS_DAVID = (['HAM','BRE','LBC', 'HAJ'], ['AGP','MAD','BIO'])
+        self.AIRPORTS_PILAR = (['AGP','GRX'], ['MUC','FMM','NUE'])
+        self.AIRPORTS_DAVID = (['MUC','FMM','NUE'], ['AGP','MAD','BIO', 'GRX'])
         self.DAYS_PILAR = ([4,5], [7])  # Each of the lists corresponds to each flight of the round trip
         self.DAYS_DAVID = ([3,4,5], [7,8])  # 1 is Monday, 7 is Sunday. Higher numbers correpond to the next week
         
